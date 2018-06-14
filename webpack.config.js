0c82bb8cea3206444c5c50d78774e177dc2cce6e
WuDiXiaoShuaiCai <876991244@qq.com>
2018-06-14 22:39:33 +0800

    source map

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',   //source map追踪错误
     plugins: [
         new CleanWebpackPlugin(['dist']),  //构建之前先清空
         new HtmlWebpackPlugin({ //根据入口文件与title自动生成index.html，对已有html进行覆盖
