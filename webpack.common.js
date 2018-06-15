0a8e3070ad5401ede3578ecec71b3c3519760b86
WuDiXiaoShuaiCai <876991244@qq.com>
2018-06-15 16:30:26 +0800

    生产环境分离

---
+++ webpack.common.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',  //Minify PNG, JPEG, GIF, SVG and WEBP images with imagemin
+                        options: {
+                          bypassOnDebug: true, // webpack@1.x
+                          disable: true, // webpack@2.x and newer
+                          mozjpeg: {
+                            progressive: true,
+                            quality: 65
+                          },
+                          // optipng.enabled: false will disable optipng
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: '65-90',
+                            speed: 4
+                          },
+                          gifsicle: {
+                            interlaced: false,
+                          },
+                          // the webp option will enable WEBP
+                          webp: {
+                            quality: 75
+                          }
+                        },
+                      },
+                ]
+           },
+           {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,  //字体
+              use: [
+                'file-loader'
+              ]
+            },                                     //数据
+            { 
+                test: /\.(csv|tsv)$/,
+                use: [
+                  'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                  'xml-loader'
+                ]
+            }
+        ]
+    }
+}
